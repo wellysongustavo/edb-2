41aeb8d056406fb105addb038c402f17180dceb9
Wellyson Oliveira <wellysongustavo@gmail.com>
2019-10-16 21:58:02 -0300

    implmentando classes de arvore e nós

---
+++ HuffmanCoding/src/br/com/edb/huffmanCoding/BinaryTree.java
@@ -0,0 +1,104 @@
+package br.com.edb.huffmanCoding;
+
+public class BinaryTree {
+    private Node[] nodes; //manda nodes
+    private int size;//quantos elementos tem
+    private int capacity;//quantos elementos cabem
+
+    public BinaryTree() {
+        this(10);
+    }
+
+    public BinaryTree(int capacity) {
+        nodes = new Node[capacity];
+        this.size = 0;
+        this.capacity = capacity;
+    }
+
+    public void insert(char letter, int frequency) {
+        insert(new Node(letter, frequency));
+    }
+
+    public void insert(Node node) {
+        ensureCapacity();
+        nodes[getSize()] = node;
+        heapifyUp(getSize());
+        size++;
+    }
+
+    private void heapifyUp(int index) {
+        int parentIndex = getParentIndex(index);
+
+        if (parentIndex < 0) {
+            return;
+        }
+
+        Node pai    = nodes[parentIndex];
+        Node node = nodes[index];
+
+        if (node.getFrequency() > pai.getFrequency()) {
+            nodes[index]   = pai;
+            nodes[parentIndex] = node;
+            heapifyUp(parentIndex);
+        }
+    }
+
+    public int getParentIndex(int index) {
+        return (int) Math.floor((index - 1) / 2);
+    }
+
+    private void ensureCapacity() {
+        if (size == capacity) {
+            nodes = Arrays.copyOf(nodes, capacity * 2);
+            capacity = capacity * 2;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Node peek() {
+        if (getSize() == 0) {
+            return null;
+        }
+        return nodes[0];
+    }
+
+    public void remove() {
+        nodes[0] = nodes[getSize() - 1];
+        nodes[getSize() - 1] = null;
+        size--;
+        heapifyDown(0);
+    }
+
+    private void heapifyDown(int index) {
+        int leftChild = index * 2 + 1;
+        int rightChild = index * 2 + 2;
+
+        int childIndex = -1;
+        if (leftChild < getSize()) {
+            childIndex = leftChild;
+        }
+
+        if (childIndex == -1) {
+            return;
+        }
+
+        if (rightChild < getSize()) {
+            if (nodes[rightChild].getFrequency() > nodes[leftChild].getFrequency()) {
+                childIndex = rightChild;
+            }
+        }
+
+        if (nodes[index].getFrequency() < nodes[childIndex].getFrequency()) {
+            Node tmp          = nodes[index];
+            nodes[index]      = nodes[childIndex];
+            nodes[childIndex] = tmp;
+            heapifyDown(childIndex);
+        }
+    }
+}
+
+
+
---
+++ HuffmanCoding/src/br/com/edb/huffmanCoding/Node.java
@@ -0,0 +1,24 @@
+package br.com.edb.huffmanCoding;
+
+public class Node {
+
+    private char letter;
+    private int frequency;
+    private Node left = null;
+    private Node right = null;
+
+    public Node(char letter, int frequency) {
+        this.letter = letter;
+        this.frequency = frequency;
+    }
+
+    public char getLetter() { return letter; }
+    public int getFrequency() { return frequency; }
+    public void setFrequency(int frequency) { this.frequency = frequency; }
+
+    public Node getLeft() { return left; }
+    public void setLeft(Node left) { this.left = left; }
+    public void setRight(Node right) { this.right = right; }
+    public Node getRight() { return right; }
+
+}
